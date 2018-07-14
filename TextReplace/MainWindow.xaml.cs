011fc41dc0cabcd492b48f92598cf57195089cb7
YasakaShun <sdshvedujbfxye@gmail.com>
2018-07-14 20:12:05 +0900

    プロジェクトを作成、Rubyスクリプトを実行するところまで作成

---
+++ TextReplace/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using IronRuby;
+using Microsoft.Scripting.Hosting;
+using System.Windows;
+
+namespace TextReplace
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ScriptEngine engine = Ruby.CreateEngine();
+            engine.ExecuteFile("Test.rb");
+        }
+    }
+}
